38ffe7a9bf24dbc1f6c6e1ff70e716fbafe9642d
Morgan McLaughlin <fozefy@gmail.com>
2019-04-15 20:38:04 -0400

    Moved results writing to common location

--- eldrazi_tron.py
+++ eldrazi_tron.py
@@ -1,5 +1,6 @@
 import numpy as np
 from hand import Hand
+import resultwriter
 
 iterations = 500000
 starting_size = 7 #inclusive
@@ -15,6 +16,8 @@ totals = np.zeros((starting_size + 1) - mullto)
 land_names = ["Cavern of Souls", "Sea Gate Wreckage", "Wastes", "Eldrazi Temple", "Urza's Mine", "Urza's Power Plant", "Urza's Tower", "Ghost Quarter"]
 
 for i in range(iterations):
+	if i % 1000 == 0:
+		print(str(i) + " of " + str(iterations))
 	flag = False
 	early_flag = False
 	for j in range(0,(starting_size + 1) - mullto):
@@ -48,36 +51,7 @@ p_hands = hand_counts / totals.reshape((starting_size + 1) - mullto,1)
 p_early_success = early_success / iterations
 p_success = success / iterations
 
-with open("output/etron_output.csv","wb") as file:
-	file.write(str(iterations) + " iterations\n\n")
-
-	file.write("Probabilities of good hands:\n")
-	for size in range(mullto,8):
-		file.write(str(size) + ",")
-	file.write("\n")
-	for p in p_good:
-		file.write(str(p) + ",")
-	file.write("\n\n")
-
-	# hand type headers
-	file.write("hand sizes/types,")
-	for type in hand_types:
-		file.write(str(type) + ",")
-
-	#results
-	file.write("\n")
-	for size in reversed(range(len(p_hands))):
-		file.write(str(size + mullto) + ",")
-		for p in p_hands[size,:]:
-			 file.write(str(p) + ",")
-		file.write("\n")
-
-	file.write("\n")
-	file.write("p of good hand by 5\n")
-	file.write(str(p_early_success) + "\n")
-	file.write("p of good hand by 3\n")
-	file.write(str(p_success) + "\n")
-	file.close()
+resultwriter.output_results("output/etron_output.csv", iterations, mullto, p_good, hand_types, p_hands, p_early_success, p_success)
 
 print(p_good)
 print(np.flip(p_hands, axis = 0))
