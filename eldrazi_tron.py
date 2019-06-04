465e0337988d23fd513f18e8e0851d62e978cee5
Morgan McLaughlin <fozefy@gmail.com>
2019-06-04 00:43:28 -0400

    Converted more files to new format
    ETron, Rock and KarnTron now converted.
    
    Also removed the old common result writer.

--- eldrazi_tron.py
+++ eldrazi_tron.py
@@ -1,59 +1,39 @@
 import numpy as np
 from hand import Hand
-import resultwriter
+from mulliganTester import MulliganTester
 
-iterations = 500000
-starting_size = 7 #inclusive
-mullto = 3	#not inclusive
-hand_types = ["Tron", "Chalice + 2 Lands", "t3 TKS", "t2 Reshaper", "2 Temples + Spell"]
-hand = Hand("decklists/e_tron.txt")
-success = 0.0
-early_success = 0.0
-good_counts = np.zeros((starting_size + 1) - mullto)
-hand_counts = np.zeros(((starting_size + 1) - mullto,len(hand_types)))
-totals = np.zeros((starting_size + 1) - mullto)
+class ETronMullTester(MulliganTester):
+    hand_types = ["Tron", "Chalice + 2 Lands", "t3 TKS", "t2 Reshaper", "2 Temples + Spell"]
+    hand = Hand("decklists/e_tron.txt")
+    output_file = "output/etron_output__test.csv"
 
-land_names = ["Cavern of Souls", "Sea Gate Wreckage", "Wastes", "Eldrazi Temple", "Urza's Mine", "Urza's Power Plant", "Urza's Tower", "Ghost Quarter"]
+    def __init__(self):
+        MulliganTester.__init__(self)
+        self.land_names = ["Cavern of Souls", "Sea Gate Wreckage", "Wastes", "Eldrazi Temple", "Urza's Mine", "Urza's Power Plant", "Urza's Tower", "Ghost Quarter"]
 
-for i in range(iterations):
-	if i % 1000 == 0:
-		print(str(i) + " of " + str(iterations))
-	flag = False
-	early_flag = False
-	for j in range(0,(starting_size + 1) - mullto):
-		hand.new_hand(starting_size - j)
+    def run(self):
+        self.iterations = 1000
+        self.runParis()
+        self.printResults()
 
-		tron = hand.has_tron()
-		lands = hand.count_of(land_names)
-		has_TKS = hand.contains("Thought-Knot Seer")
-		has_Reshaper = hand.contains("Matter Reshaper")
-		has_Chalice = hand.contains("Chalice of the Void")
-		temples = hand.count_of("Eldrazi Temple") 
-		has_Map = float(hand.count_of("Expedition Map") > 0 and lands >= 2)
-		has_Stone = hand.contains("Mind Stone")
+    def CheckHand(self):
+        hand = self.hand
+        
+        tron = hand.has_tron()
+        lands = hand.count_of(self.land_names)
+        has_TKS = hand.contains("Thought-Knot Seer")
+        has_Reshaper = hand.contains("Matter Reshaper")
+        has_Chalice = hand.contains("Chalice of the Void")
+        temples = hand.count_of("Eldrazi Temple") 
+        has_Map = float(hand.count_of("Expedition Map") > 0 and lands >= 2)
+        has_Stone = hand.contains("Mind Stone")
 
-		t3_TKS = has_TKS * (((has_Map or (temples > 0)) * (lands >= 2)) or (has_Stone * (lands >= 3)))
+        t3_TKS = has_TKS * (((has_Map or (temples > 0)) * (lands >= 2)) or (has_Stone * (lands >= 3)))
 		# tron, Chalice + lands, t3 TKS, t2 Reshaper, 2 temples and any spell
-		results = np.array([tron, has_Chalice * (lands >= 2), t3_TKS, has_Reshaper * (temples > 0) * (lands >= 2), (temples + has_Map*(lands >= 2) >= 2) * (lands < (7 - j))])
-		good_counts[starting_size - j - mullto] += (np.sum(results) > 0)
-		hand_counts[starting_size - j - mullto,:] += results
-		totals[starting_size - j - mullto] += 1
-		if not flag:
-			if (np.sum(results) > 0):
-				flag = True
-				if(j <= 2):
-					early_flag = True
-	success += flag
-	early_success += early_flag
+        results = np.array([tron, has_Chalice * (lands >= 2), t3_TKS, has_Reshaper * (temples > 0) * (lands >= 2), (temples + has_Map*(lands >= 2) >= 2) * (lands < (hand.handsize()))])
 
-p_good = good_counts / totals
-p_hands = hand_counts / totals.reshape((starting_size + 1) - mullto,1)
-p_early_success = early_success / iterations
-p_success = success / iterations
+        return results
 
-resultwriter.output_results("output/etron_output.csv", iterations, mullto, p_good, hand_types, p_hands, p_early_success, p_success)
-
-print(p_good)
-print(np.flip(p_hands, axis = 0))
-print(p_early_success)
-print(p_success)
+if __name__ == "__main__":
+    eTronTester = ETronMullTester()
+    eTronTester.run() 
--- karn_tron.py
+++ karn_tron.py
@@ -1,93 +1,68 @@
 import numpy as np
 from hand import Hand
-import resultwriter
+from mulliganTester import MulliganTester
 
-iterations = 500000
-starting_size = 7 #inclusive
-mullto = 4	#not inclusive
-hand_types = ["T3KarnGG", "Tron+payoff", "Tron", "solid_keep"]
-hand = Hand("decklists/karn_tron.txt")
-success = 0.0
-early_success = 0.0
-good_counts = np.zeros((starting_size + 1) - mullto)
-hand_counts = np.zeros(((starting_size + 1) - mullto,len(hand_types)))
-totals = np.zeros((starting_size + 1) - mullto)
+class KarnTronMullTester(MulliganTester):
 
-green_source = ["Forest", "Chromatic Sphere", "Chromatic Star"]
-tron1 = ["Urza's Power Plant"]
-tron2 = ["Urza's Tower"]
-tron3 = ["Urza's Mine"]
-stir = ["Ancient Stirrings"]
-sylv = ["Sylvan Scrying"]
-exmap = ["Expedition Map"]
-payoff = ["Walking Ballista", "Karn Liberated", "Wurmcoil Engine"]
-karn = ["Karn Liberated"]
-lands = ["Wastes", "Forest", "Urza's Power Plant", "Urza's Tower", "Urza's Mine"]
-cantrips = ["Chromatic Sphere", "Chromatic Star", "Relic of Progenitus"]
+    hand_types = ["T3KarnGG", "Tron+payoff", "Tron", "solid_keep"]
+    hand = Hand("decklists/karn_tron.txt")
+    output_file = "output/karntron_output___test.csv"
 
-def CheckHand(hand):
-    #print("")
-    #print (hand.hand_as_str())
-    numTron = hand.contains(tron1) + hand.contains(tron2) + hand.contains(tron3)
-    hasGreen = hand.contains(green_source)
-    numStir = hand.count_of(stir) if hasGreen else 0
-    numPayoff = hand.count_of(payoff)
-    hasKarn = hand.contains(karn)
-    tronFinders = hand.count_of(exmap) + hand.count_of(sylv) if hasGreen else 0
-    numLand = hand.count_of(lands)
-    numCantrips = hand.count_of(cantrips) + numStir
+    def __init__(self):
+        MulliganTester.__init__(self)
+        self.green_source = ["Forest", "Chromatic Sphere", "Chromatic Star"]
+        self.tron1 = ["Urza's Power Plant"]
+        self.tron2 = ["Urza's Tower"]
+        self.tron3 = ["Urza's Mine"]
+        self.stir = ["Ancient Stirrings"]
+        self.sylv = ["Sylvan Scrying"]
+        self.exmap = ["Expedition Map"]
+        self.payoff = ["Walking Ballista", "Karn Liberated", "Wurmcoil Engine"]
+        self.karn = ["Karn Liberated"]
+        self.lands = ["Wastes", "Forest", "Urza's Power Plant", "Urza's Tower", "Urza's Mine"]
+        self.cantrips = ["Chromatic Sphere", "Chromatic Star", "Relic of Progenitus"]
 
-    t3karnGG = False
-    tronWPayoff = False
-    justTron = False
-    keepable = False
+    def run(self):
+        self.iterations = 1000
+        self.runParis()
+        self.printResults()
 
-    if numTron >= 1 and numLand >= 2 and numTron + tronFinders >= 3:
-        if hasKarn:
-            t3karnGG = True
-        elif numPayoff > 0:
-            tronWPayoff = True
-        else:
-            justTron = True
-    elif numLand >= 2 and numCantrips >= 2 and numPayoff >= 1 and numPayoff < 4:
-        keepable = True
-    elif numLand >= 1 and numCantrips > 2:
-        keepable = True
+    def CheckHand(self):
+        #print("")
+        #print (hand.hand_as_str())
 
-    results = np.array([t3karnGG, tronWPayoff, justTron, keepable])
-    #print(str (results))
-    return results
+        hand = self.hand
 
-for i in range(iterations):
-    if i % 5000 == 0:
-        print(str(i) + " of " + str(iterations))
+        numTron = hand.contains(self.tron1) + hand.contains(self.tron2) + hand.contains(self.tron3)
+        hasGreen = hand.contains(self.green_source)
+        numStir = hand.count_of(self.stir) if hasGreen else 0
+        numPayoff = hand.count_of(self.payoff)
+        hasKarn = hand.contains(self.karn)
+        tronFinders = hand.count_of(self.exmap) + hand.count_of(self.sylv) if hasGreen else 0
+        numLand = hand.count_of(self.lands)
+        numCantrips = hand.count_of(self.cantrips) + numStir
 
-    flag = False
-    early_flag = False
-    for j in range(0,(starting_size + 1) - mullto):
-        hand.new_hand(starting_size - j)
+        t3karnGG = False
+        tronWPayoff = False
+        justTron = False
+        keepable = False
 
-        results = CheckHand(hand)
-        
-        good_counts[starting_size - j - mullto] += (np.sum(results) > 0)
-        hand_counts[starting_size - j - mullto,:] += results
-        totals[starting_size - j - mullto] += 1
-        if not flag:
-            if (np.sum(results) > 0):
-                flag = True
-                if(j <= 2):
-                    early_flag = True
-    success += flag
-    early_success += early_flag
+        if numTron >= 1 and numLand >= 2 and numTron + tronFinders >= 3:
+            if hasKarn:
+                t3karnGG = True
+            elif numPayoff > 0:
+                tronWPayoff = True
+            else:
+                justTron = True
+        elif numLand >= 2 and numCantrips >= 2 and numPayoff >= 1 and numPayoff < 4:
+            keepable = True
+        elif numLand >= 1 and numCantrips > 2:
+            keepable = True
 
-p_good = good_counts / totals
-p_hands = hand_counts / totals.reshape((starting_size + 1) - mullto,1)
-p_early_success = early_success / iterations
-p_success = success / iterations
+        results = np.array([t3karnGG, tronWPayoff, justTron, keepable])
+        #print(str (results))
+        return results
 
-resultwriter.output_results("output/karntron_output.csv", iterations, mullto, p_good, hand_types, p_hands, p_early_success, p_success)
-
-print(p_good)
-print(np.flip(p_hands, axis = 0))
-print(p_early_success)
-print(p_success)
+if __name__ == "__main__":
+    karnTester = KarnTronMullTester()
+    karnTester.run() 
