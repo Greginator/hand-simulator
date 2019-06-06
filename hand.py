48c5ac025cc8464f62382d27eb0c58d37db9e4c5
Morgan McLaughlin <fozefy@gmail.com>
2019-06-06 01:08:05 -0400

    Implemented London mulligan test function
    Added new functions to hand to support subsetting a hand to allow us to run the London mulligan test.
    
    This function can now manage testing over all hands giving player choice of which card to put back.

--- hand.py
+++ hand.py
@@ -2,92 +2,121 @@ import numpy as np
 from collections import Counter
 import scipy.stats as sps
 
+
 class Hand:
-	def __init__(self, deck_name):
-		if deck_name:
-			self.deck, self.decklist = self.process(deck_name)
-		else:
-			self.deck = ["Storm Crow"] * 60
-			self.decklist = dict(Counter(self.deck))
+    def __init__(self, deck_name):
+        if deck_name:
+            self.deck, self.decklist = self.process(deck_name)
+        else:
+            self.deck = ["Storm Crow"] * 60
+            self.decklist = dict(Counter(self.deck))
 
-	def hand_as_str(self):
-		return str(self.card_counts)
+    def hand_as_str(self):
+        return str(self.card_counts)
 
-	def process(self, filename):
-		with open(filename,"r") as file:
-			file_read = [line.strip().split(" ", 1) for line in file if len(line.split()) > 0]
-		deck = []
-		for [number, card] in file_read:
-			deck += [card] * int(number)
-		decklist = dict(Counter(deck))
-		return deck, decklist
+    def process(self, filename):
+        with open(filename, "r") as file:
+            file_read = [line.strip().split(" ", 1)
+                         for line in file if len(line.split()) > 0]
+        deck = []
+        for [number, card] in file_read:
+            deck += [card] * int(number)
+        decklist = dict(Counter(deck))
+        return deck, decklist
 
-	def set_deck(self, new_deck):
-		if isinstance(new_deck, list):
-			self.deck = new_deck
-			self.decklist = dict(Counter(new_deck))
-		elif isinstance(new_deck, dict):
-			deck = []
-			for card, number in new_deck.items():
-				deck += [card] * int(number)
-			self.deck = deck
-			self.decklist = dict(Counter(new_deck))
-		elif isinstance(new_deck, str):
-			self.deck, self.decklist = self.process(new_deck)
-		else:
-			print("ERROR: invalid deck format")
-			return False
-		return True
+    def set_deck(self, new_deck):
+        if isinstance(new_deck, list):
+            self.deck = new_deck
+            self.decklist = dict(Counter(new_deck))
+        elif isinstance(new_deck, dict):
+            deck = []
+            for card, number in new_deck.items():
+                deck += [card] * int(number)
+            self.deck = deck
+            self.decklist = dict(Counter(new_deck))
+        elif isinstance(new_deck, str):
+            self.deck, self.decklist = self.process(new_deck)
+        else:
+            print("ERROR: invalid deck format")
+            return False
+        return True
 
-	def get_deck(self):
-		return self.deck.copy()
+    def get_deck(self):
+        return self.deck.copy()
 
-	def get_decklist(self):
-		return dict(self.decklist.copy())
+    def get_decklist(self):
+        return dict(self.decklist.copy())
 
-	def new_hand(self, size = 7):
-		self.size = size
-		draws = np.random.choice(len(self.deck),size,replace = False)
-		self.card_counts = Counter([self.deck[i] for i in draws])
-		self.cards = set(self.card_counts.keys())
-		return self.card_counts.copy()
+    def num_subets(self):
+        return self.num_subsets
 
-	def set_hand(self, newHand):
-		self.card_counts = Counter([newHand])
-		self.cards = set(self.card_counts.keys())
+    def nextSubset(self):
+        self.subsetIndex += 1
+        self.card_counts = Counter(self.subsetHands[self.subsetIndex])
+        self.cards = set(self.card_counts.keys())
 
-	def has_tron(self):
-		check = [0] * 4
-		if "Urza's Tower" in self.cards:
-			check[0] = 1
-		if "Urza's Mine" in self.cards:
-			check[1] = 1
-		if "Urza's Power Plant" in self.cards:
-			check[2] = 1
-		if "Expedition Map" in self.cards:
-			check[3] = 1
-		return float(sum(check) >= 3)
+    def generate_subset_hands(self, numHide):
+        if numHide > 2:
+            raise Exception('not implemented for more than 2')
 
-	def contains(self,cards):
-		if isinstance(cards, str):
-			return 1.0*(cards in self.cards)
-		return float(len(self.cards & set(cards)) > 0)
+        counterAsList = sorted(self.card_counts.elements())
+        self.subsetHands = []
+        for i in range(0, self.size):
+            subHand = counterAsList[:i] + counterAsList[i+1:]
+            if numHide > 1:
+                for j in range(i, self.size-1):
+                    subHand = counterAsList[:j] + counterAsList[j+1:]
+                    self.subsetHands.append(subHand)
+            else:
+                self.subsetHands.append(subHand)
 
-	def count_of(self,cards):
-		if isinstance(cards, str):
-			return float(self.card_counts[cards])
-		return float(sum([self.card_counts[card] for card in cards]))
+        self.num_subsets = len(self.subsetHands)
+        self.subsetIndex = -1
+        self.size = self.size - numHide
 
-	def handsize(self):
-		return self.size
+    def new_hand(self, size=7):
+        self.size = size
+        draws = np.random.choice(len(self.deck), size, replace=False)
+        self.card_counts = Counter([self.deck[i] for i in draws])
+        self.cards = set(self.card_counts.keys())
+        return self.card_counts.copy()
 
-	def expect(self,cards,size = 7):
-		if isinstance(cards, str):
-			n = self.decklist[cards]
-		elif isinstance(cards, list):
-			n = sum([self.decklist[card] for card in cards])
-		else: 
-			return 0
-		if n == 0: return 0
-		return sps.hypergeom.mean(len(self.deck),n,size)
+    def set_hand(self, newHand):
+        self.card_counts = Counter([newHand])
+        self.cards = set(self.card_counts.keys())
 
+    def has_tron(self):
+        check = [0] * 4
+        if "Urza's Tower" in self.cards:
+            check[0] = 1
+        if "Urza's Mine" in self.cards:
+            check[1] = 1
+        if "Urza's Power Plant" in self.cards:
+            check[2] = 1
+        if "Expedition Map" in self.cards:
+            check[3] = 1
+        return float(sum(check) >= 3)
+
+    def contains(self, cards):
+        if isinstance(cards, str):
+            return 1.0*(cards in self.cards)
+        return float(len(self.cards & set(cards)) > 0)
+
+    def count_of(self, cards):
+        if isinstance(cards, str):
+            return float(self.card_counts[cards])
+        return float(sum([self.card_counts[card] for card in cards]))
+
+    def handsize(self):
+        return self.size
+
+    def expect(self, cards, size=7):
+        if isinstance(cards, str):
+            n = self.decklist[cards]
+        elif isinstance(cards, list):
+            n = sum([self.decklist[card] for card in cards])
+        else:
+            return 0
+        if n == 0:
+            return 0
+        return sps.hypergeom.mean(len(self.deck), n, size)
--- mulliganTester.py
+++ mulliganTester.py
@@ -5,12 +5,8 @@ class MulliganTester(ABC):
     def __init__(self):
         self.iterations = 500000
         self.starting_size = 7
-        self.mullto = 4
-        self.success = 0.0
-        self.early_success = 0.0
-        self.good_counts = np.zeros((self.starting_size + 1) - self.mullto)
-        self.hand_counts = np.zeros(((self.starting_size + 1) - self.mullto, len(self.hand_types)))
-        self.totals = np.zeros((self.starting_size + 1) - self.mullto)
+        self.mullto = 5
+        self.resetCounters()
 
     @property
     @abstractmethod
@@ -31,7 +27,15 @@ class MulliganTester(ABC):
     def CheckHand(self):
         pass
 
+    def resetCounters(self):
+        self.success = 0.0
+        self.early_success = 0.0
+        self.good_counts = np.zeros((self.starting_size + 1) - self.mullto)
+        self.hand_counts = np.zeros(((self.starting_size + 1) - self.mullto, len(self.hand_types)))
+        self.totals = np.zeros((self.starting_size + 1) - self.mullto)
+
     def runParis(self):
+        self.resetCounters()
         for i in range(self.iterations):
             if i % 5000 == 0:
                 print(str(i) + " of " + str(self.iterations))
@@ -54,17 +58,42 @@ class MulliganTester(ABC):
             self.success += flag
             self.early_success += early_flag
 
+    def getBestResult(self, results):
+        best = None
+        bestIndex = None
+        for res in results:
+            if True in res:
+                if best is None:
+                    best = res
+                    bestIndex = np.argmax(best)
+                    if bestIndex == 0:
+                        break
+                else:
+                    testIndex = np.argmax(res)
+                    if testIndex < bestIndex:
+                        best = res
+        if best is None:
+            best = results[0]
+        return best
+
     def runLondon(self):
+        self.resetCounters()
         for i in range(self.iterations):
             if i % 5000 == 0:
                 print(str(i) + " of " + str(self.iterations))
-
             flag = False
             early_flag = False
             for j in range(0,(self.starting_size + 1) - self.mullto):
-                self.hand.new_hand(self.starting_size - j)
-
-                results = self.CheckHand()
+                self.hand.new_hand(self.starting_size)
+                if j > 0:
+                    subResults = []
+                    self.hand.generate_subset_hands(j)
+                    for k in range(0, self.hand.num_subsets):
+                        self.hand.nextSubset()
+                        subResults.append(self.CheckHand())
+                    results = self.getBestResult(subResults)
+                else:
+                    results = self.CheckHand()
                 
                 self.good_counts[self.starting_size - j - self.mullto] += (np.sum(results) > 0)
                 self.hand_counts[self.starting_size - j - self.mullto,:] += results
@@ -78,6 +107,7 @@ class MulliganTester(ABC):
             self.early_success += early_flag
 
     def runVancouver(self):
+        self.resetCounters()
         for i in range(self.iterations):
             if i % 5000 == 0:
                 print(str(i) + " of " + str(self.iterations))
@@ -100,8 +130,9 @@ class MulliganTester(ABC):
             self.success += flag
             self.early_success += early_flag
 
-    def output_results(self, p_good, p_hands, p_early_success, p_success):
+    def output_results(self, p_good, p_hands, p_early_success, p_success, header):
         with open(self.output_file,"w") as file:
+            file.write(header)
             file.write(str(self.iterations) + " iterations\n\n")
 
             file.write("Probabilities of good hands:\n")
@@ -132,13 +163,13 @@ class MulliganTester(ABC):
             file.write(str(p_success) + "\n")
             file.close()
 
-    def printResults(self):
+    def printResults(self, header):
         p_good = self.good_counts / self.totals
         p_hands = self.hand_counts / self.totals.reshape((self.starting_size + 1) - self.mullto,1)
         p_early_success = self.early_success / self.iterations
         p_success = self.success / self.iterations
 
-        self.output_results(p_good, p_hands, p_early_success, p_success)
+        self.output_results(p_good, p_hands, p_early_success, p_success, header)
 
         print(p_good)
         print(np.flip(p_hands, axis = 0))
